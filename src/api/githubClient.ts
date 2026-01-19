84ab9217883ce3fc633ada87fb95043e06a0d40e
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-19 08:56:58 +0200

    github Client

---
+++ src/api/githubClient.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const GITHUB_API_BASE = "https://api.github.com";
+
+export class GitHubClient {
+  private headers: Record<string, string>;
+
+  constructor(token?: string) {
+    this.headers = token
+      ? { Authorization: `token ${token}` }
+      : {};
+  }
+
+  private async getAllPaginated(endpoint: string): Promise<string[]> {
+    const results: string[] = [];
+    let page = 1;
+
+    while (true) {
+      const response = await axios.get(
+        `${GITHUB_API_BASE}${endpoint}`,
+        {
+          headers: this.headers,
+          params: { per_page: 100, page }
+        }
+      );
+
+      if (response.data.length === 0) break;
+
+      for (const user of response.data) {
+        results.push(user.login);
+      }
+
+      page++;
+    }
+
+    return results;
+  }
+
+  getFollowers(username: string): Promise<string[]> {
+    return this.getAllPaginated(`/users/${username}/followers`);
+  }
+
+  getFollowing(username: string): Promise<string[]> {
+    return this.getAllPaginated(`/users/${username}/following`);
+  }
+}
